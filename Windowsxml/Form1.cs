17a09c719dc8b24bdbb2f3370c3c344d6418ed34
Emre Küntaş <kuntasemre@gmail.com>
2019-01-04 09:27:21 +0300

    Update Form1.cs

--- Windowsxml/Form1.cs
+++ Windowsxml/Form1.cs
@@ -20,7 +20,18 @@ namespace Windowsxml
         {
             InitializeComponent();
         }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
 
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
        
         private void button1_Click(object sender, EventArgs e)
         {
